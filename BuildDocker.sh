b42804659a4ce726289e38cc272c388bb8c1e469
sirliu <sirliu110@Gmail.com>
2022-04-29 09:32:14 +0800

    Update BuildDocker.sh

--- BuildDocker.sh
+++ BuildDocker.sh
@@ -1,4 +1,6 @@
+cd /tmp
 wget https://github.com/curlconverter/curlconverter.github.io/archive/refs/heads/gh-pages.zip
 unzip gh-pages.zip
 docker build -t curlconverter:latest .
 docker build -f Arm64Dockerfile -t curlconverter:arm64 .
+rm ./gh-pages -rf
